159ba189a486844ddfa91f10d1db1b75418399d8
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-01-23 23:40:46 +0530

    cors

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
  
 const bodyParser = require('body-parser');
- 
+ const cors =  require("cors");
 const dotenv =  require("dotenv");
 const { MongoConnect } = require('./db/db.js');
 const authrouter = require('./routes/authRoute.js');
 const Doctorrouter = require('./routes/doctorRoutes.js');
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT|| 3001;
-
+app.use(cors());
  
 
 app.use(bodyParser.json());
